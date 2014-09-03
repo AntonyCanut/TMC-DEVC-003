cc29ec3b9bca8ac5e7f2aa6e7d5297efcd3221b9
northvik <camillepire@northvik.fr>
2014-09-03 10:29:42 +0200

    fond

--- SpaceInvader/Main.c
+++ SpaceInvader/Main.c
@@ -10,10 +10,14 @@ SDL_Renderer *Renderer;
 
 SDL_Surface *sBackground;
 SDL_Texture *tBackground;
+SDL_Surface *sEarth;
+SDL_Texture *tEarth;
 
 SDL_Rect bPos;
+SDL_Rect ePos;
 
-char *srcBackgroundPicture = "img/decor/fond_4800x4800.png";
+char *srcBackgroundPicture = "img/decor/fond_1920x1080.png";
+char *srcEarthPicture = "img/decor/earth_1700x3800.png";
 
 void Init()
 {
@@ -29,15 +33,23 @@ void Init()
     bPos.y = 0;
     bPos.w = SCREEN_WIDTH;
     bPos.h = SCREEN_HEIGHT;
+
+    ePos.x = 0;
+    ePos.y = 0;
+    ePos.w = SCREEN_WIDTH;
+    ePos.h = SCREEN_HEIGHT;
 }
 
 void Load()
 {
-    tBackground = IMG_LoadTexture(Renderer, "img/decor/fond_4800x4800.png");
+    tEarth = IMG_LoadTexture(Renderer, "img/decor/earth_1700x3800.png");
+    tBackground = IMG_LoadTexture(Renderer, "img/decor/fond_1920x1080.png");
 }
 
 void Destroy()
 {
+    SDL_DestroyTexture(tEarth);
+    SDL_FreeSurface(sEarth);
     SDL_DestroyTexture(tBackground);
     SDL_FreeSurface(sBackground);
     SDL_DestroyRenderer(Renderer);
@@ -47,7 +59,15 @@ void Destroy()
 
 void Update()
 {
+    if (ePos.y < 600)
+    {
+        ePos.y += 1;
+    }else{
+        ePos.y = 0;
+    }
+
     SDL_RenderClear(Renderer);
+    SDL_RenderCopy(Renderer, tEarth, 0, &ePos);
     SDL_RenderCopy(Renderer, tBackground, 0, &bPos);
     SDL_RenderPresent(Renderer);
 }

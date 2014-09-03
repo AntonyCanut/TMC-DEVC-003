f5055e97c81fb4ce5338ff7f36e2624c7bd5beaa
northvik <camillepire@northvik.fr>
2014-09-03 14:17:29 +0200

    structure en place

--- SpaceInvader/Moon.c
+++ SpaceInvader/Moon.c
@@ -5,39 +5,40 @@
 //  Created by Camille Pire on 20/08/14.
 //  Copyright (c) 2014 Antony Canut. All rights reserved.
 //
+#include "header.h"
 
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include "SpriteFunc.h"
-
-struct Moon *Moon;
-
-SDL_Surface* LoadMoon(){
-    SDL_Surface *sMoon;
-    sMoon= SDL_LoadBMP("img/decor/lune_3200x16000_P_3200.png");
-    return sMoon;
+void LoadContentMoon(){
+    tMoon= IMG_LoadTexture(Renderer, "img/decor/lune_3200x16000_P_3200.png");
 }
 
 void UnLoadMoon(){
-    SDL_FreeSurface(LoadMoon());
+    SDL_DestroyTexture(tMoon);
 }
 
-void DrawMoon();
+void DrawMoon(){
+    SDL_RenderCopy(Renderer, tMoon, 0, &Moon->Position);
+}
 
-void UpdateMoon();
+void UpdateMoon(){}
 
-void DeadMoon();
+void DeadMoon(){}
+
+void DestroyMoon(){
+    UnLoadMoon();
+    free(Moon);
+}
 
 
-void InitMoon(Coordonnees position){
-    Moon->Position.x = -100;
-    Moon->Position.y = 800;
-    Moon->Position.w = 100;
-    Moon->Position.h = 100;
-    
-    Moon->LoadContent = LoadContentMoon();
-    Moon->UnLoadContent = UnLoadContentMoon();
-    Moon->Draw = DrawMoon();
-    Moon->Update = UpdateMoon();
-    Moon->Dead = DeadMoon();
+void InitMoon(){
+    Moon = (struct MoonStruct *) malloc(sizeof(struct MoonStruct) + 1);
+    Moon->Position.x = 0;
+    Moon->Position.y = 0;
+    Moon->Position.w = 500;
+    Moon->Position.h = 500;
+
+    // Moon->LoadContent = LoadContentMoon();
+    // Moon->UnLoadContent = UnLoadContentMoon();
+    // Moon->Draw = DrawMoon();
+    // Moon->Update = UpdateMoon();
+    // Moon->Dead = DeadMoon();
 }
---
+++ SpaceInvader/background.c
@@ -0,0 +1,56 @@
+//
+//  background.c
+//  SpaceInvader
+//
+//  Created by Camille Pire on 20/08/14.
+//  Copyright (c) 2014 Antony Canut. All rights reserved.
+//
+#include "header.h"
+
+void LoadContentBackground(){
+    tEarth = IMG_LoadTexture(Renderer, "img/decor/earth_1700x3800.png");
+    tStar = IMG_LoadTexture(Renderer, "img/decor/fond_1920x1080.png");
+}
+
+void UnLoadBackground(){
+    SDL_DestroyTexture(tStar);
+    SDL_DestroyTexture(tEarth);
+}
+
+void DrawBackground(){
+    SDL_RenderCopy(Renderer, tEarth, 0, &Background->PositionEarth);
+    SDL_RenderCopy(Renderer, tStar, 0, &Background->PositionStar);
+}
+
+void UpdateBackground(){
+    if (Background->PositionEarth.y < 600)
+    {
+        Background->PositionEarth.y += 1;
+    }else{
+        Background->PositionEarth.y = 0;
+    }
+}
+
+void DestroyBackground(){
+    UnLoadBackground();
+    free(Background);
+}
+
+
+void InitBackground(){
+    Background = (struct BackgroundStruct *) malloc(sizeof(struct BackgroundStruct) + 1);
+    Background->PositionStar.x = 0;
+    Background->PositionStar.y = 0;
+    Background->PositionStar.w = SCREEN_WIDTH;
+    Background->PositionStar.h = SCREEN_HEIGHT;
+
+    Background->PositionEarth.x = 0;
+    Background->PositionEarth.y = 0;
+    Background->PositionEarth.w = SCREEN_WIDTH;
+    Background->PositionEarth.h = SCREEN_HEIGHT;
+
+    // Background->LoadContent = LoadContentBackground();
+    // Background->UnLoadContent = UnLoadContentBackground();
+    // Background->Draw = DrawBackground();
+    // Background->Update = UpdateBackground();
+}

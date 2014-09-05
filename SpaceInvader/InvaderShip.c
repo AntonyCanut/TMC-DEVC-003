cff08c4c191f00e1f12e69913cb41bd494030358
Chloé LIBAN <liban_c@etna-alternance.net>
2014-09-05 17:03:12 +0200

    Invadeeerrss

--- SpaceInvader/InvaderShip.c
+++ SpaceInvader/InvaderShip.c
@@ -2,6 +2,7 @@
 
 int countUpdateInvaderShip = 0;
 int deadInvaderShip        = 0;
+bool *rightInvader;
 
 void LoadContentInvaderShip() {
     tInvaderShip = IMG_LoadTexture(Renderer, "img/invader/invader1_600x3600_P_600.png");
@@ -16,45 +17,61 @@ void DrawInvaderShip() {
 }
 
 void UpdateInvaderShip() {
-    // if (countUpdateInvaderShip % 3 == 0){
-    //     if (InvaderShip->Part.x <= 0)
-    //     {
-    //         InvaderShip->Part.x = 600;
-    //     }else if (InvaderShip->Part.x >= 600)
-    //     {
-    //         InvaderShip->Part.x == 0;
-    //     }
-    // }
-
-    if (countUpdateInvaderShip > 30)
-    {
-        deadInvaderShip = 1;
+    if (countUpdateInvaderShip % 17 == 0) {
+        if (InvaderShip->Part.x <= 0) {
+            InvaderShip->Part.x = 600;
+        } else if (InvaderShip->Part.x >= 600) {
+            InvaderShip->Part.x = 0;
+        }
     }
-    DeadInvaderShip();
-    countUpdateInvaderShip += 1;
+
+    if (countUpdateInvaderShip % 2 == 0) {
+        if (rightInvader == true) {
+            InvaderShip->Position.x += 5;
+            if (InvaderShip->Position.x >= (SCREEN_WIDTH - 70)) {
+                rightInvader = false;
+                InvaderShip->Position.x -= 5;
+                InvaderShip->Position.y += 70;
+            }
+        } else {
+            InvaderShip->Position.x -= 5;
+            if (InvaderShip->Position.x <= 10) {
+                rightInvader = true;
+                InvaderShip->Position.x += 5;
+                InvaderShip->Position.y += 70;
+            }
+        }
+    }
+
+    // if (countUpdateInvaderShip == 30)
+    // {
+    //     deadInvaderShip = 1;
+    // }
+    // DeadInvaderShip();
+    countUpdateInvaderShip += 1;
 }
 
 void ShotInvaderShip() {}
 
 void DeadInvaderShip() {
     if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 1) {
-        InvaderShip->Part.x = 1200;
-        deadInvaderShip     = 2;
-    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 2) {
-        InvaderShip->Part.x = 1800;
-        deadInvaderShip     = 3;
-    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 3) {
-        InvaderShip->Part.x = 2400;
-        deadInvaderShip     = 4;
-    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 4) {
-        InvaderShip->Part.x = 3000;
-        deadInvaderShip     = 5;
-    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 5) {
-        deadInvaderShip = 0;
-        DestroyInvaderShip();
-    }
+        InvaderShip->Part.x = 1200;
+        deadInvaderShip     = 2;
+    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 2) {
+        InvaderShip->Part.x = 1800;
+        deadInvaderShip     = 3;
+    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 3) {
+        InvaderShip->Part.x = 2400;
+        deadInvaderShip     = 4;
+    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 4) {
+        InvaderShip->Part.x = 3000;
+        deadInvaderShip     = 5;
+    } else if (countUpdateInvaderShip % 5 == 0 && deadInvaderShip == 5) {
+        deadInvaderShip = 0;
+        DestroyInvaderShip();
+    }
 }
-
+    
 void DestroyInvaderShip() {
     UnLoadInvaderShip();
     free(InvaderShip);
@@ -63,10 +80,12 @@ void DestroyInvaderShip() {
 
 void InitInvaderShip() {
     InvaderShip = (struct InvaderShipStruct *) malloc(sizeof(struct InvaderShipStruct) + 1);
-    InvaderShip->Position.x = 5;
+    InvaderShip->Position.x = 11;
     InvaderShip->Position.y = 5;
     InvaderShip->Position.w = 70;
     InvaderShip->Position.h = 70;
+
+    rightInvader = true;
 
     InvaderShip->Part.x = 0;
     InvaderShip->Part.y = 0;

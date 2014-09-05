f959856c2afe5c175526ca472d73cbb43e282273
carrierev <valentin.carriere@gmail.com>
2014-09-05 14:11:44 +0200

    Ajout library
    Ajout library mixer et music stage

--- SpaceInvader/Main.c
+++ SpaceInvader/Main.c
@@ -1,6 +1,10 @@
 
 #include "header.h"
 
+Mix_Music *musique; 
+   Mix_Chunk *sonBackground;//Créer un pointeur pour stocker un .WAV
+   Mix_Chunk *son2;
+
 void InitMain()
 {
     if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
@@ -15,6 +19,17 @@ void InitMain()
     left=false;
     shoot=false;
 
+    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) == -1) //Initialisation de l'API Mixer
+    {
+        printf("%s", Mix_GetError());
+    }
+    
+    Mix_AllocateChannels(32); //Allouer 32 canaux
+    sonBackground = Mix_LoadWAV("music/stage.wav"); //Charger un wav dans un pointeur
+    son2 = Mix_LoadWAV("music/stage.wav");
+    Mix_VolumeChunk(sonBackground, MIX_MAX_VOLUME); //Mettre un volume pour ce wav
+    Mix_VolumeChunk(son2, MIX_MAX_VOLUME);
+    Mix_PlayChannel(2, sonBackground, -1);
     InitBackground();
     InitMoon();
     InitMars();
@@ -37,6 +52,9 @@ void DestroyMain()
     DestroyBackground();
     SDL_DestroyRenderer(Renderer);
     SDL_DestroyWindow(Window);
+    Mix_FreeChunk(sonBackground);//Libération du son 1
+    Mix_FreeChunk(son2);
+    Mix_CloseAudio(); //Fermeture de l'API
     SDL_Quit();
     exit(0);
 }
